5a58f7c023e93e446c2c2c5e3c39a6d902d3da03
Hackbright Student <info@hackbrightacademy.com>
2016-04-15 12:37:44 -0700

    Refactored to use __init__ from the Abstract class and set attributes in the subclasses.

--- melons.py
+++ melons.py
@@ -3,15 +3,26 @@
 class AbstractMelonOrder(object):
     """AbstractMelonOrder"""
 
-    def __init__(self, species, qty, country_code, tax, order_type):
+    def __init__(self, species, qty, country_code=None):
         """Initialize our abstract melon order"""
-        
+
         self.species = species
         self.qty = qty
-        self.shipped = False
-        self.tax = tax
-        self.order_type = order_type
         self.country_code = country_code
+        self.shipped = False
+        #if country_code:
+        #   self.country_code = country_code
+
+    # yesterday's implementaiton:
+    # def __init__(self, species, qty, country_code, tax, order_type):
+    #     """Initialize our abstract melon order"""
+        
+    #     self.species = species
+    #     self.qty = qty
+    #     self.shipped = False
+    #     self.tax = tax
+    #     self.order_type = order_type
+    #     self.country_code = country_code
 
     def get_total(self):
         """Calculate price."""
@@ -33,53 +44,27 @@ class AbstractMelonOrder(object):
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes"""
+    #country_code = "US"
+    tax = 0.08
+    order_type = "domestic"
 
-        # Call dunder init of the superclasses of the current class and pass in parameters
-        super(DomesticMelonOrder, self).__init__(species, qty, "US", 0.08, "domestic")
-        # self.order_type = "domestic"
-        # self.tax = 0.08
+    # yesterday's implementation:
+    # def __init__(self, species, qty):
+    #     """Initialize melon order attributes"""
 
-    # def get_total(self):
-    #     """Calculate price."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Set shipped to true."""
-
-    #     self.shipped = True
-
+    #     # Call dunder init of the superclasses of the current class and pass in parameters
+    #     super(DomesticMelonOrder, self).__init__(species, qty, "US", 0.08, "domestic")
+        
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes"""
+    tax = 0.17
+    order_type = "international"
 
-        # Call dunder init of the superclasses of the current class and pass in parameters
-        super(InternationalMelonOrder, self).__init__(species, qty, country_code, 0.17, "international")
+    #yesterday's implementation:
+    # def __init__(self, species, qty, country_code):
+    #     """Initialize melon order attributes"""
 
-        #self.country_code = country_code
-        # self.order_type = "international"
-        # self.tax = 0.17
-
-    # def get_total(self):
-    #     """Calculate price."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Set shipped to true."""
-
-    #     self.shipped = True
-
-    # def get_country_code(self):
-    #     """Return the country code."""
-
-    #     return self.country_code
+    #     # Call dunder init of the superclasses of the current class and pass in parameters
+    #     super(InternationalMelonOrder, self).__init__(species, qty, country_code, 0.17, "international")
